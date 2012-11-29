e75e99a3daefc5da84150901d0068c3a578e8a56
ejucah <e.jucah@ieee.org>
2012-11-29 21:56:47 -0200

    First commit

--- src/FuzzyParking/FPLC.java
+++ src/FuzzyParking/FPLC.java
@@ -18,7 +18,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
