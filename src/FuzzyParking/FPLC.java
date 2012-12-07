ac2c42cd9cf805f09f463f19ac07ac5c50979f56
ejucah <e.jucah@ieee.org>
2012-12-07 10:30:44 -0200

    License and copyright update

--- src/FuzzyParking/FPLC.java
+++ src/FuzzyParking/FPLC.java
@@ -4,6 +4,25 @@ package FuzzyParking;
  * Desenvolvido para o projeto da disciplina IA861, Sistemas Nebulosos
  * DAC, FEEC, Unicamp, segundo semestre de 2012.
  * @author Eduardo F. Jucá de Castro
+ * 
+ * Copyright © 2012 Eduardo Ferreira Jucá de Castro (e.jucah@ieee.og)
+ * 
+ * Este arquivo é parte do programa FuzzyParking.
+ * 
+ * FuzzyParking é um software livre; você pode redistribui-lo e/ou 
+ * modifica-lo dentro dos termos da Licença Pública Geral GNU como 
+ * publicada pela Fundação do Software Livre (FSF); na versão 2 da 
+ * Licença, ou qualquer versão.
+ * 
+ * Este programa é distribuido na esperança que possa ser  útil, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de 
+ * ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. 
+ * Veja a Licença Pública Geral GNU para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral 
+ * GNU junto com este programa, se não, escreva para a Fundação 
+ * do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
  */
 import java.io.InputStream;
 import java.net.URL;
